e50815118a8336b75981645abcdfa83fc40ece65
siluye <seife@yekicomp.com>
2022-12-01 12:58:44 -0800

    Python Import Module assignment

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+def add(a, b):
+
+    """Addition function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+
+    """Subtraction function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+
+    """Multiplication function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+
+    """Division function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
